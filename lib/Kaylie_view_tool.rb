ac43b93edda62dfd00c2703e751e0cceff7c386f
Kaylie Hollander <kphollander1@gmail.com>
2017-07-19 14:07:28 +0000

    Fixed the ruby gem

---
+++ lib/Kaylie_view_tool.rb
@@ -0,0 +1,7 @@
+require "Kaylie_view_tool/version"
+require "Kaylie_view_tool/renderer"
+
+
+module KaylieViewTool
+  # Your code goes here...
+end
---
+++ lib/Kaylie_view_tool/version.rb
@@ -0,0 +1,3 @@
+module KaylieViewTool
+  VERSION = "0.1.0"
+end
